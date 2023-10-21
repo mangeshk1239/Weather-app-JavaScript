53e3da078b1be77762db595161712a8f2721047f
Mangesh Kamble <mangeshkamble1239@gmail.com>
2023-10-22 00:44:38 +0530

    changes in UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,13 @@ import imagesMap from './assets/weatherImagesHashMap';
 function App() {
   const [cityName, setCityName] = React.useState("");
   const [data, setData] = React.useState(null);
-  const [backgroundImage, setBackgroundImage] = React.useState(null);
+  const [backgroundImage, setBackgroundImage] = React.useState("https://images.pexels.com/photos/296234/pexels-photo-296234.jpeg?auto=compress&cs=tinysrgb&w=1600");
 
   const styles = {
     backgroundImage: `url(${backgroundImage})`,
+    width: "100%",
+    height: "100px",
+    padding: "100px"
   };
 
   React.useEffect(() => {
@@ -19,11 +22,17 @@ function App() {
   }, [data]);
 
   return (
-    <div className="weatherApp" style={styles}>
-      <M.TextField onChange={(e) => setCityName(e.target.value)} id="outlined-basic" label="Outlined" variant="outlined" />
-      <M.Button variant="contained" onClick={handleGetWeather}>Submit</M.Button>
-    </div>
-  )
+    <M.Box sx={styles}>
+      <M.Grid container spacing={2} sx={{ alignItems: "center" }}>
+        <M.Grid item xs={6} md={8}>
+          <M.TextField onChange={(e) => setCityName(e.target.value)} id="outlined-basic" label="Enter any City name" variant="outlined" />
+        </M.Grid>
+        <M.Grid item xs={6} md={4}>
+          <M.Button variant="contained" onClick={handleGetWeather}>Submit</M.Button>
+        </M.Grid>
+      </M.Grid>
+    </M.Box>
+  );
 
   async function handleGetWeather() {
     try {
