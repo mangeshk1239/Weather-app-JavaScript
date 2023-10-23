6611788e14fed3aa47f147d0701659b11abbb2f8
Mangesh Kamble <mangeshkamble1239@gmail.com>
2023-10-23 19:04:22 +0530

    changes in UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,45 +7,45 @@ function App() {
 
   const styles = {
     width: "100%",
-    height: "100px",
-    padding: "100px"
+    padding: "50px",
+    backgroundColor: "blue",
+    borderRadius: "25px",
+    textAlign: "center"
   };
 
   return (
     <M.Box sx={styles}>
       <M.Grid container spacing={2} sx={{ alignItems: "center" }}>
-        <M.Grid item xs={6} md={8}>
-          <M.TextField onChange={(e) => setCityName(e.target.value)} id="outlined-basic" label="Enter any City name" variant="outlined" />
+        <M.Grid item xs={8} md={8} lg={8}>
+          <M.TextField fullWidth onChange={(e) => setCityName(e.target.value)} id="outlined-basic" label="Enter any City name" variant="outlined" sx={{ borderRadius: "50px" }} />
         </M.Grid>
-        <M.Grid item xs={6} md={4}>
+        <M.Grid item xs={4} md={4} lg={4}>
           <M.Button variant="contained" onClick={handleGetWeather}>Submit</M.Button>
         </M.Grid>
       </M.Grid>
       {
         data != null
           ?
-          <>
-            <M.Grid container spacing={2} sx={{ alignItems: "center" }}>
-              <M.Grid item xs={12} md={12}>
-                <img src={data.cityWeatherIcon} width={100} height={100} />
-              </M.Grid>
-              <M.Grid item xs={12} md={12}>
-                <M.Typography variant="h1" gutterBottom>
-                  {data.cityTemp}
-                </M.Typography>
-              </M.Grid>
-              <M.Grid item xs={12} md={12}>
-                <M.Typography variant="h1" gutterBottom>
-                  {data.cityName}
-                </M.Typography>
-              </M.Grid>
-              <M.Grid item xs={12} md={12}>
-                <M.Typography variant="h1" gutterBottom>
-                  {data.cityWeather}
-                </M.Typography>
-              </M.Grid>
+          <M.Grid container spacing={2} sx={{ alignItems: "center" }}>
+            <M.Grid item xs={12} md={12}>
+              <M.Typography variant="h5" gutterBottom>
+                {data.cityWeather.charAt(0).toUpperCase() + data.cityWeather.slice(1)}
+              </M.Typography>
             </M.Grid>
-          </>
+            <M.Grid item xs={12} md={12}>
+              <img src={data.cityWeatherIcon} width={"20%"} />
+            </M.Grid>
+            <M.Grid item xs={12} md={12}>
+              <M.Typography variant="h3" gutterBottom>
+                {data.cityTemp}&deg;C
+              </M.Typography>
+            </M.Grid>
+            <M.Grid item xs={12} md={12} sx={{ padding: "0", margin: "0" }}>
+              <M.Typography variant="h3" gutterBottom>
+                {data.cityName}
+              </M.Typography>
+            </M.Grid>
+          </M.Grid>
           :
           <></>
       }
